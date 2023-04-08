aa55bb3158dbf77b58ec0d423ff4fcfc89d42cf2
Parham Rahimi <parham7992@gmail.com>
2023-04-08 22:44:14 +0200

    synced changes in dd_package and updated and new unit test

--- test/unittests/test_dd_operator_reduction.cpp
+++ test/unittests/test_dd_operator_reduction.cpp
@@ -24,7 +24,7 @@ TEST(DDReduceOperator, DDReduceTranspose_Test1) {
     auto regular_matrix = ddo->getMatrix(regular_dd);
     EXPECT_EQ(inputMatrix, regular_matrix);
 
-    const auto reduced_dd = ddo->reduceTranspose(regular_dd);
+    const auto reduced_dd = ddo->reduceEdgeOperation(regular_dd, 1);
     // TODO: extend other print mat function in package class
     // std::cout << "redconstructed matrix of reduced dd:" << std::endl;
     // ddo->printMatrix(reduced_dd);
@@ -46,5 +46,34 @@ TEST(DDReduceOperator, DDReduceTranspose_Test1) {
 
 }
 
+TEST(DDReduceOperator, DDReduceX_Test1) {
+    // Unitary matrix
+    const auto inputMatrix = dd::CMat {{dd::SQRT2_2, dd::SQRT2_2, 0, 0},
+                                        {-dd::SQRT2_2, dd::SQRT2_2, 0, 0},
+                                        {0, 0, dd::SQRT2_2, -dd::SQRT2_2},
+                                        {0, 0, dd::SQRT2_2, dd::SQRT2_2}};
 
+    const dd::QubitCount nrQubits = 2;
+    const auto ddo = std::make_unique<dd::Package<>>(nrQubits);
+
+    dd::mEdge regular_dd = ddo->makeDDFromMatrix(inputMatrix);
+    std::cout << "-----------------------------" << std::endl;
+    std::cout << "redconstructed matrix of regular dd:" << std::endl;
+    ddo->printMatrix(regular_dd);
+    std::cout << std::endl;
+    auto regular_matrix = ddo->getMatrix(regular_dd);
+    EXPECT_EQ(inputMatrix, regular_matrix);
+
+    const auto reduced_dd = ddo->reduceEdgeOperation(regular_dd, 3);
+    // TODO: extend other print mat function in package class
+    // std::cout << "redconstructed matrix of reduced dd:" << std::endl;
+    // ddo->printMatrix(reduced_dd);
+    // ddo->printMatrix(regular_dd);
+    // std::cout << std::endl;
+    auto reduced_dd_matrix = ddo->getMatrix(reduced_dd);
+    EXPECT_EQ(inputMatrix, reduced_dd_matrix);
+
+    std::cout << "-----------------------------" << std::endl;
+
+}
 
